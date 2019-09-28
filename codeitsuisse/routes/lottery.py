49597ba745d14b8de252236653f91b388328b494
Jonathan Lim <limjonathanzs@gmail.com>
2019-09-28 12:51:08 +0800

    x

--- codeitsuisse/routes/lottery.py
+++ codeitsuisse/routes/lottery.py
@@ -1,6 +1,7 @@
 import logging
 import json
 
+
 from flask import request, jsonify;
 
 from codeitsuisse import app;
@@ -10,8 +11,14 @@ logger = logging.getLogger(__name__)
 @app.route('/lottery', methods=['POST'])
 def lottery():
     data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+    print(data)
+    #logging.info("data sent for evaluation {}".format(data))
+
+    guess_array = [10,9,8,7,6,5,4,3,2,1]
+    # guess_array = np.random.randint(1, 101, 10)
+    print(guess_array)
+
+    #inputValue = data.get("input")
+    #result = inputValue * inputValue
+    #logging.info("My result :{}".format(result))
+    return json.dumps(guess_array)
