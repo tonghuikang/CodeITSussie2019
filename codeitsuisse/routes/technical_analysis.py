f5ec2028187542245a232986cb6ab6e8e7177cc3
tonghuikang <huikang_tong@mymail.sutd.edu.sg>
2019-09-29 02:40:58 +0800

    auto

--- codeitsuisse/routes/technical_analysis.py
+++ codeitsuisse/routes/technical_analysis.py
@@ -34,17 +34,34 @@ def optimise_case(arr_in):
         yy = numpy.array(yy)
         ff = numpy.fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
         Fyy = abs(numpy.fft.fft(yy))
-        guess_freq = abs(ff[numpy.argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+        # plt.plot(Fyy[1:])
+        # plt.show()
+        guess_index = numpy.argmax(Fyy[4:])+4
+        guess_freq = abs(ff[guess_index]+1)   # excluding the zero frequency "peak", which is related to offset
+        print(numpy.argmax(Fyy[1:]))
         guess_amp = numpy.std(yy) * 2.**0.5
         guess_offset = numpy.mean(yy)
-        guess = numpy.array([guess_amp, 2.*numpy.pi*guess_freq, 0., 0., guess_offset])
+        guess = numpy.array([guess_amp, 2.*numpy.pi*guess_freq, 0., 0.,
+                            guess_amp, 2.*numpy.pi*guess_freq, 0., 0.,
+    #                          guess_amp, 2.*numpy.pi*guess_freq, 0., 0.,
+                            guess_offset])
 
-        def sinfunc(t, A, w, p, k, c):  return A * numpy.sin(w*t + p) + k*t + c
+        def sinfunc(t, 
+                    A1, w1, p1, k1, 
+                    A2, w2, p2, k2,
+    #                 A3, w3, p3, k3,
+                    c):  return (A1 * numpy.sin(w1*t + p1) + k1*t + 
+                                A2 * numpy.sin(w2*t + p2) + k2*t +
+    #                              A3 * numpy.sin(w3*t + p3) + k3*t +
+                                c)
         popt, pcov = scipy.optimize.curve_fit(sinfunc, tt, yy, p0=guess)
-        A, w, p, k, c = popt
-        f = w/(2.*numpy.pi)
-        fitfunc = lambda t: A * numpy.sin(w*t + p) + k*t + c
-        return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": numpy.max(pcov), "rawres": (guess,popt,pcov)}
+        (A1, w1, p1, k1, 
+        A2, w2, p2, k2, 
+    #      A3, w3, p3, k3,
+        c) = popt
+        
+        fitfunc = lambda t: sinfunc(t, *popt)
+        return {"fitfunc": fitfunc, "maxcov": numpy.max(pcov), "rawres": (guess,popt,pcov)}
     
     larger_range = 1099
 
