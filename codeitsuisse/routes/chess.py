c0ed9996bdaea956aac30d0a1071fd171b1ed9a4
tonghuikang <huikang_tong@mymail.sutd.edu.sg>
2019-09-29 09:42:07 +0800

    auto

--- codeitsuisse/routes/chess.py
+++ codeitsuisse/routes/chess.py
@@ -14,31 +14,37 @@ def branch():
     branch = data["branch_officers_timings"]
     free = [0 for _ in range(len(branch))]
     customer = data["N"]
-
-    for _ in range(len(branch)):
+    print(customer)
+    
+    for i in range(customer):
         cst = free.index(min(free))
         free[cst] = free[cst] + branch[cst]
-        print(free)
-
-    answer = cst
-    return jsonify({"answer" : answer})
-
-
-@app.route('/secretmessage', methods=['POST'])
-def message():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    branch = data["branch_officers_timings"]
-    free = [0 for _ in range(len(branch))]
-    customer = data["N"]
-
-    for i in range(len(N)):
-        cst = free.index(min(free))
-        free[cst] = free[cst] + branch[cst]
-        print(free)
+        # print(free)
         if i > 100000:
             break
     
     answer = cst
     print(answer)
     return jsonify({"answer" : answer})
+
+
+@app.route('/secretmessage', methods=['POST'])
+def message():
+    # data = request.get_json()
+    # logging.info("data sent for evaluation {}".format(data))
+    # branch = data["branch_officers_timings"]
+    # free = [0 for _ in range(len(branch))]
+    # customer = data["N"]
+    # print(customer)
+
+    # for i in range(customer):
+    #     cst = free.index(min(free))
+    #     free[cst] = free[cst] + branch[cst]
+    #     print(free)
+    #     if i > 100000:
+    #         break
+    
+    # answer = cst
+    # print(answer)
+    # return jsonify({"answer" : answer})
+    pass
