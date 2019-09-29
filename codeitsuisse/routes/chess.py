ef3786077e98c2b3a7562f44545388dee47c520c
tonghuikang <huikang_tong@mymail.sutd.edu.sg>
2019-09-29 09:40:16 +0800

    auto

--- codeitsuisse/routes/chess.py
+++ codeitsuisse/routes/chess.py
@@ -18,6 +18,27 @@ def branch():
     for _ in range(len(branch)):
         cst = free.index(min(free))
         free[cst] = free[cst] + branch[cst]
+        print(free)
+
+    answer = cst
+    return jsonify({"answer" : answer})
+
+
+@app.route('/secretmessage', methods=['POST'])
+def message():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    branch = data["branch_officers_timings"]
+    free = [0 for _ in range(len(branch))]
+    customer = data["N"]
+
+    for i in range(len(N)):
+        cst = free.index(min(free))
+        free[cst] = free[cst] + branch[cst]
+        print(free)
+        if i > 100000:
+            break
     
     answer = cst
+    print(answer)
     return jsonify({"answer" : answer})
