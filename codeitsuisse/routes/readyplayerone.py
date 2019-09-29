8b43087849e41ffb3e1a196c277277518e175cde
Jonathan Lim <limjonathanzs@gmail.com>
2019-09-29 10:48:20 +0800

    4th attempt at ReadyPlayerOne; hard coding with a proposed winning algo; added an impossible test case

--- codeitsuisse/routes/readyplayerone.py
+++ codeitsuisse/routes/readyplayerone.py
@@ -18,7 +18,8 @@ def readyplayerone():
     Jar_1 = list(range(1, N_integer + 1, 1))
     Jar_2 = []
     calculated = strategy(N_integer, T_totaldesired, Jar_1, Jar_2)
-    return Response(json.dumps(calculated), mimetype='application/json')
+    output = {"res" : calculated}
+    return Response(json.dumps(output), mimetype='application/json')
 
 
 def strategy(N_integer, T_totaldesired , Jar_1 , Jar_2):
@@ -26,11 +27,13 @@ def strategy(N_integer, T_totaldesired , Jar_1 , Jar_2):
     if N_integer >= T_totaldesired:
         return 1                # Player 1 wins on the first turn
 
-    if T_totaldesired == (N_integer*(N_integer + 1) / 2):              #Case of requiring the perfect AP Sum
+    if T_totaldesired == (N_integer*(N_integer + 1) / 2):        # Case of requiring the perfect AP Sum
         if N_integer % 2 == 0:
             return -1                   # Player 2 will always win (Even number of tries)
         return N_integer                # Player 1 will always win (Odd number of tries)
 
+    if T_totaldesired > (N_integer*(N_integer + 1) / 2):        # Impossible case of having more than possible
+        return -1
 
     while (len(Jar_2) < N_integer and T_totaldesired - sum(Jar_2) > max(Jar_1)):    #Fixing up the empty max arg
         ideal_number = T_totaldesired - sum(Jar_2) - min(Jar_1) - max(Jar_1)
